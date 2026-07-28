8286b83a43bf23827e682f9de113204b00172669
Mohammed Yasser <memomohammed.2005@gmail.com>
2026-07-28 14:35:14 +0300

    pp

--- backend/upload_pipeline.py
+++ backend/upload_pipeline.py
@@ -3,16 +3,22 @@ from pathlib import Path
 sys.path.append(str(Path(__file__).resolve().parent.parent))
 
 from ai_modules.document_ai.pipeline import process_document
-path=("data/raw/contractA.pdf")
+from ai_modules.legal_nlp.pipeline import sections_entities_pipeline
+
+path=("../data/raw/contractC.pdf")
 
 def upload_pipeline(pdf_path: str | Path):
 
-    process_document(pdf_path)
+    result=process_document(pdf_path)
+    full_text= result['full_text']
+    pages=result['pages']   
+
+    result=sections_entities_pipeline(full_text)
+    sections=result['sections']
+    entities=result['entities']
 
 
 
+    return sections,entities
 
-
-    return "mohammed"
-
-upload_pipeline(path)
+print(upload_pipeline(path))
